10b7d74565e5ae823f847fa0e852710ecbc14946
Edwin Garcia <edwinprogramando@gmail.com>
2023-06-23 10:10:27 -0500

    que es el estado

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,17 +1,20 @@
+import { useState } from 'react';
 import './TodoSearch.css';
 
-const TodoSearch = () => (
-<input
-    className="TodoSearch"
-    type="text"
-    placeholder="Cortar la cebolla"
-    onChange={event => {
-        console.log('escribiste en el todo search');
-        console.log(event);
-        console.log(event.target);
-        console.log(event.target.value);
-    }}
-/>
-);
+const TodoSearch = () => {
+    const [textSearch, setTextSearch] = useState('');
+
+    return(
+        <input
+            className="TodoSearch"
+            type="text"
+            placeholder="Cortar la cebolla"
+            value={textSearch}
+            onChange={event => {
+                setTextSearch(event.target.value);
+            }}
+        />
+    );
+}
 
 export { TodoSearch };
